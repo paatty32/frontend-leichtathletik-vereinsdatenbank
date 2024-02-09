16f74f659ae330ad5304ea24467a6f746c38e3fb
paatty32 <patrick.boadu.boafo@hotmail.de>
2024-02-09 16:03:18 +0100

    WIP - displaying athlete in cards by clicking the search button

--- src/app/athlete-search/startpassnummer/startpassnummer.component.ts
+++ src/app/athlete-search/startpassnummer/startpassnummer.component.ts
@@ -1,15 +1,34 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Athlete } from '../../athlete';
+import { AthleteCardComponent } from '../../athlete-card/athlete-card.component';
 
 @Component({
   selector: 'la-startpassnummer',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, AthleteCardComponent],
   templateUrl: './startpassnummer.component.html',
   styleUrl: './startpassnummer.component.css'
 })
 export class StartpassnummerComponent {
 
   startpassnummer: number | null = null;
+  athletes: Array<Athlete> = []
+
+  search(): void {
+      const testAthlete = {
+
+        startpassnummer: 1234,
+        name: 'Patrick',
+        surname: 'Boadu',
+        ageGroup: 'Hauptklasse',
+
+      }
+
+      this.athletes.push(testAthlete);
+  }
 
 }
+
+
