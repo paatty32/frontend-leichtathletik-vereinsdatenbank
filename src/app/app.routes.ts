3786421a05aced9d4a8335757227d93f89fa31cd
patty32 <patrick.boadu.boafo@hotmail.de>
2024-02-11 23:36:34 +0100

    UI ADD - new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { StartpassnummerComponent } from './athlete-search/startpassnummer/startpassnummer.component';
+import { NameFormComponent } from './athlete-search/name-form/name-form.component';
 
 export const routes: Routes = [
     {
         path:'startpassnummer',
         component: StartpassnummerComponent
+
+    },
+
+    {
+        path:'name',
+        component: NameFormComponent
     }
 ];
