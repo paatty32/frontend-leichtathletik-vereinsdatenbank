c9800fdf4d54e5fd933edf3f01d478b0ad5320bb
patty32 <patrick.boadu.boafo@hotmail.de>
2024-02-17 20:17:45 +0100

    Adding new routing to navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StartpassnummerComponent } from './athlete-search/startpassnummer/startpassnummer.component';
 import { NameFormComponent } from './athlete-search/name-form/name-form.component';
+import { AgeGroupFormComponent } from './athlete-search/age-group-form/age-group-form.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     {
         path:'name',
         component: NameFormComponent
+    },
+
+    {
+        path:'altersklassen',
+        component: AgeGroupFormComponent
     }
 ];
