54cbd6289e85bb900aaa58a761ecf28252b665fd
patty32 <patrick.boadu.boafo@hotmail.de>
2024-03-02 00:02:07 +0100

    UI ADD - Routing to Athlete Profile and adding new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { StartpassnummerComponent } from './athlete-search/startpassnummer/startpassnummer.component';
 import { NameFormComponent } from './athlete-search/name-form/name-form.component';
 import { AgeGroupFormComponent } from './athlete-search/age-group-form/age-group-form.component';
+import { AthleteProfileComponent } from './athlete-profile/athlete-profile.component';
 
 export const routes: Routes = [
     {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     {
         path:'altersklassen',
         component: AgeGroupFormComponent
+    },
+
+    {
+        path:'startpassnummer/:id',
+        component: AthleteProfileComponent
     }
 ];
