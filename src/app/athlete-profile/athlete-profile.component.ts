86cf7dad3462fe90ce4c686a8bf99973d380d1a3
patty32 <patrick.boadu.boafo@hotmail.de>
2024-03-02 00:02:30 +0100

    UI ADD - creating new component for athlete profile

---
+++ src/app/athlete-profile/athlete-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'la-athlete-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './athlete-profile.component.html',
+  styleUrl: './athlete-profile.component.css'
+})
+export class AthleteProfileComponent {
+
+}
