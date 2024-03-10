ffed628be2ccf927de768e9e4d9f1d780477005a
patty32 <patrick.boadu.boafo@hotmail.de>
2024-03-10 21:40:28 +0100

    UI ADD - create new component personal best card

---
+++ src/app/athlete-profile/athlete-profile-overview/personal-best-card/personal-best-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'la-personal-best-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-best-card.component.html',
+  styleUrl: './personal-best-card.component.css'
+})
+export class PersonalBestCardComponent {
+
+}
