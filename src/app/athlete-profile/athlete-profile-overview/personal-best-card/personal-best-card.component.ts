db60ccec4067d6351364b5115ac931d3c48d3a53
patty32 <patrick.boadu.boafo@hotmail.de>
2024-03-18 23:23:44 +0100

    UI ADD - showing personal best of an athlete

--- src/app/athlete-profile/athlete-profile-overview/personal-best-card/personal-best-card.component.ts
+++ src/app/athlete-profile/athlete-profile-overview/personal-best-card/personal-best-card.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CompetitionService } from '../../competition-service.service';
+import { PersonalBest } from '../../personalBest';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'la-personal-best-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personal-best-card.component.html',
   styleUrl: './personal-best-card.component.css'
 })
-export class PersonalBestCardComponent {
+export class PersonalBestCardComponent implements OnInit {
+
+  personalBests: Array<PersonalBest> = [];
+  @Input() startpassnummer: number = 0;
+
+  constructor(private competitionService: CompetitionService){}
+  
+  
+  ngOnInit(): void {
+
+    this.getPersonalBests()
+  }
+
+  getPersonalBests(): void {
+
+    if(this.startpassnummer !== 0){
+      this.competitionService.getPersonalBestsOfAthlete(this.startpassnummer).subscribe(
+        data => {
+          this.personalBests = data;
+          console.log(typeof this.personalBests[0].result)
+        }
+      )
+    } else {
+      console.log('There is no startpassnummer', this.startpassnummer)
+    }
+
+
+  }
 
 }
