c4569f22bc610384e8693bbf3dac06794f062996
patty32 <patrick.boadu.boafo@hotmail.de>
2024-04-30 23:00:28 +0200

    UI ADD - adding details section to competitino results

--- src/app/athlete-profile/athlete-profile-result/athlete-profile-result.component.ts
+++ src/app/athlete-profile/athlete-profile-result/athlete-profile-result.component.ts
@@ -4,50 +4,41 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
-import { CompetitionResult } from './CompetitionResult';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { CompetitionEntry, CompetitionResult } from './CompetitionResult';
+import { NgbCollapseModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'la-athlete-profile-result',
   standalone: true,
-  imports: [FormsModule, CommonModule, NgbCollapseModule],
+  imports: [FormsModule, CommonModule, NgbCollapseModule, NgbTooltipModule],
   templateUrl: './athlete-profile-result.component.html',
   styleUrl: './athlete-profile-result.component.css'
 })
 export class AthleteProfileResultComponent implements OnInit {
-
-
 
   seasonYears: Array<number> = [];
   selectedCompYear: number = 0;
 
   startpassnummer: number = 0;
 
-  competitionsResults: CompetitionResult =  {key1: []};
+  competitionsResults: CompetitionResult = {key1: []};
 
   isCollapsed: boolean = true;
-
 
   constructor(private competitionService: CompetitionService, private route: ActivatedRoute){
 
   }
   
   ngOnInit(): void {
-
     this.route.params.subscribe(
       param => {
-
         this.startpassnummer = param['startpassnummer']
-      
       }
     );
-
     this.initializeFormSelect();
-
   }
 
-  initializeFormSelect(){
-
+  private initializeFormSelect(){
     this.competitionService.getCompetitionYears(this.startpassnummer).pipe(
       switchMap(
         years => {
@@ -59,26 +50,22 @@ export class AthleteProfileResultComponent implements OnInit {
 
         })
     ).subscribe(competitions => {
-
-      this.competitionsResults = competitions;
-
-    });
+        this.competitionsResults = competitions;
+      });
   }
 
   onSelectChange($event: Event) {
-
     this.competitionService.getCompetitionResultsByYear(this.startpassnummer, this.selectedCompYear).subscribe(
-      
       competitions => {
-
         this.competitionsResults = competitions;
-
     });
-    }
+  }
 
-    onClick() {
+  onDetailsClick(value: CompetitionEntry) {
+    value.collapseDetails = !value.collapseDetails;
+  }
 
-      this.isCollapsed = !this.isCollapsed;
-
-    }
+  detailIsEmpty(value: CompetitionEntry){
+    return value.ageGroup == null && (value.resultLink == 'nan' || value.resultLink == '-')
+  }
 }
